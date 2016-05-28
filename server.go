c41ed3601ae82d04ca0c85216485b04fb1dff8ee
simonf <simon@home.simonf.net>
2016-05-28 21:51:50 +0100

    Finally got it working

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package monitor_listener
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,7 +13,8 @@ func StartServer(port int) {
 	ph := http.HandlerFunc(handler)
 	sh := http.FileServer(http.Dir("."))
 	http.Handle("/index.html", ph)
-	http.Handle("/*", sh)
+	http.Handle("/", sh)
+	fmt.Printf("Listening for HTTP on %d\n", port)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
 
@@ -51,37 +53,44 @@ func sendHTMLResponse(w http.ResponseWriter) {
       <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <title>Monitor</title>
-      <link rel='stylesheet', href='/bootstrap-3.3.6-dist/css/bootstrap.min.css'/>
-      <link rel='stylesheet', href='/font-awesome-4.5.0/css/font-awesome.min.css'/>
-      <link rel='stylesheet', href='/stylesheets/style.css'/>
-      <script src='/javascripts/jquery-1.12.1.min.js'/>
-      <script src='/bootstrap-3.3.6-dist/js/bootstrap.min.js'/>
+      <link rel='stylesheet' href='/public/bootstrap-3.3.6-dist/css/bootstrap.min.css'/>
+      <link rel='stylesheet' href='/public/font-awesome-4.5.0/css/font-awesome.min.css'/>
+      <link rel='stylesheet' href='/public/stylesheets/style.css'/>
+      <script src='/public/javascripts/jquery-1.12.1.min.js'></script>
+      <script src='/public/bootstrap-3.3.6-dist/js/bootstrap.min.js'></script>
     </head>
     <body>
-      <div class='content'>
-        <div class='container-fluid'>
-          <h1>Monitor</h1>
-          <div class='row'>
+    <div class='content'>
+      <div class='container-fluid'>
+        <h1>Monitor</h1>
+        <div class='row'>
           {{range .}}
           <div class='col-xs12 col-sm-12 col-md-12 col-lg-6'>
             <div class='row'>
               <div class='col-xs-12 col-sm-12 col-md-12' id='agent'>
                 <div class='{{ .Status }}'>
-                  <h3><a data-target='#{{ .Name }}' data-toggle='collapse'  href='#{{ .Name }}'>{{ .Name }} &nbsp; {{ .Status }}</a></h3>
+                  <h3>
+                    <a
+                      data-target='#{{ .Name }}'
+                      data-toggle='collapse'
+                      href='#{{ .Name }}'>
+                      {{ .Name }} &nbsp; {{ .Status }}
+                    </a>
+                  </h3>
                   <p>{{ .IP }}<br/>{{ .Updated }}</p>
                   {{if .Status }}
                     <div class='collapse' id='{{ .Name }}'>
-                    {{range .Services}}
+                      {{range .Services}}
                       <div class='row'>
                         <div class='col-xs-12 col-sm-12 col-md-12'>
                           <p>
-                            <a class='{{if .Status}}btn btn-lg btn-block btn-success{{else}}btn btn-lg btn-block btn-danger{{end}}' title='{{ .Name }}'>
-                              <span class='{{ .Status }}' title='dummy'>{{ .Name}}</span>
+                            <a class='btn btn-lg btn-block btn-danger' title='{{.Name}}'>
+                              <span class='{{ .Status }}' title='dummy'>{{.Name}}</span>
                             </a>
                           </p>
                         </div>
                       </div>
-                    {{end}}
+                      {{end}}
                     </div>
                   {{end}}
                 </div>
@@ -89,9 +98,9 @@ func sendHTMLResponse(w http.ResponseWriter) {
             </div>
           </div>
           {{end}}
-         </div>
+          </div>
         </div>
-      </div>
+       </div>
     </body>
   </html>
   `
